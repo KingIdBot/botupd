d679d02a82b6d7a529d428c2e654fbd33a83ba57
KingIdBot <77149298+KingIdBot@users.noreply.github.com>
2021-02-08 18:05:05 +0800

    MR KING CHANNEL
    SUBSCRIBE

---
+++ mrking/donasi.js
@@ -0,0 +1,23 @@
+const donasi = (Ig, name) => { 
+	return 
+	 `     
+             𝗗𝗢𝗡𝗔𝗦𝗜  
+
+     *DONASI SEIKHLASNYA:)* 
+ 
+  *BOT BY 𝐌𝐑 𝐊𝐈𝐍𝐆 𝐁𝐎𝐓*
+
+
+
+
+Note:
+Jika Gamau Donasi Setidaknya Subscribe YT Gw Ngab:D
+Kalo Dah Donasi Silahkan Invit Di Group Kalian:)
+Makasih:)
+
+https://youtube.com/channel/UCHK9gIokua2zkYKYTbqAwMQ
+
+     `
+}
+
+exports.donasi = donasi
